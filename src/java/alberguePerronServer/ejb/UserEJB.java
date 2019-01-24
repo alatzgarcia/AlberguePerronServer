3e0ca112b42672d0845990882bcfbaeca61afc0c
LanderLluv <38784473+LanderLluv@users.noreply.github.com>
2019-01-24 14:20:18 +0100

    AlberguePerronServer 24/01/2019 (Working)

--- src/java/alberguePerronServer/ejb/UserEJB.java
+++ src/java/alberguePerronServer/ejb/UserEJB.java
@@ -11,12 +11,32 @@ import alberguePerronServer.exception.CreateException;
 import alberguePerronServer.exception.DeleteException;
 import alberguePerronServer.exception.ReadException;
 import alberguePerronServer.exception.UpdateException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.xml.bind.DatatypeConverter;
 
 /**
  *
@@ -46,6 +66,8 @@ public class UserEJB implements UserManagerEJBLocal{
     public void createUser(User user) throws CreateException {
         LOGGER.info("UserEJB: Creating user.");
         try{
+            user.setPassword(DatatypeConverter.printHexBinary(decrypt(DatatypeConverter.parseHexBinary(user.getPassword()))));
+            user.setPassword(DatatypeConverter.printHexBinary(getDigest(user.getPassword())));
             em.persist(user);
             LOGGER.info("UserEJB: User created.");
         }catch(Exception e){
@@ -150,5 +172,67 @@ public class UserEJB implements UserManagerEJBLocal{
             throw new ReadException(e.getMessage());
         }
         return users;
+    }
+     
+    public byte[] decrypt(byte[] pass){
+        FileInputStream fis;
+       byte[] decodedMessage = null;
+	
+        try {
+		                 		
+            fis = new FileInputStream("private.key");
+            byte[] privateKey = new byte[fis.available()];
+            fis.read(privateKey);
+		
+            PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(privateKey);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PrivateKey privKey = keyFactory.generatePrivate(privKeySpec);
+		
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.DECRYPT_MODE, privKey);
+            decodedMessage = cipher.doFinal(pass);
+                    //password = new String(decodedMessage);
+            LOGGER.info("Contraseña desencriptada");
+			
+	} catch (FileNotFoundException e) {
+			e.printStackTrace();
+	} catch (IOException e) {
+			e.printStackTrace();
+	}catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+	} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+	} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+	} catch (InvalidKeyException e) {
+			e.printStackTrace();
+	} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+	} catch (BadPaddingException e) {
+			e.printStackTrace();
+        }
+        return decodedMessage;
+    }
+    
+    public byte[] getDigest(String password) {
+        String algorithm = "SHA-512";
+        //StringBuilder stringBuilder = null;
+        byte[] result = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance(algorithm);
+            //byte[] myTextInBytes = password;
+            md.update(password.getBytes());
+            result = md.digest();
+
+            /**
+             * stringBuilder = new StringBuilder(); for (byte theByte : result)
+             * { stringBuilder.append(String.format("%02x", theByte & 0xff)); }
+			*
+             */
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
